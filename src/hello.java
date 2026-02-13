db515ad896db9b740a45a70dd9638e79b5746393
mia <artthou.reinhardt@gmail.com>
2026-02-13 09:32:06 -0500

    work?

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,31 @@
 public class hello {
     
+    public static void main(String[] args) {
+        // System.out.println("new dawn, new day");
+        weave(solid(11, 11));
+    }
+
+    public static void weave(String[][] rug) {
+        // Method to print 2D emoji arrays.
+        for(int i = 0; i < rug.length; i++) {
+            for(int j = 0; j < rug[i].length; j++) {
+                System.out.print(rug[i][j]);
+            }
+            // Break the line at the end of every row.
+            System.out.println("");
+        }
+    }
+
+    public static String[][] solid(int width, int height) {
+        String[][] rug = new String[width][height];
+
+        for(int i = 0; i < rug.length; i++) {
+            for(int j = 0; j < rug[i].length; j++) {
+                rug[i][j] = "🪻";
+            }
+        }
+
+        return rug;
+    }
+    
 }
-String a = "hello"
-{
